db92c8d3260933c4061c30052915d546dda2838b
paulo-lins <paulolins100@gmail.com>
2020-07-13 17:49:54 -0300

    movimentos de xadrez

---
+++ exercises/4_1/ex-6.js
@@ -0,0 +1,9 @@
+let chessPiece = 'CAvalo';
+let res = chessPiece.toLowerCase();
+
+if (res == 'bispo') {
+    console.log('Diagonal');
+} else
+    if (res == 'cavalo') {
+        console.log('em L');
+    }
