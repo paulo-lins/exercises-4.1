7e2632bae8c468f78d5337f0794f9e99cd7881c0
paulo-lins <paulolins100@gmail.com>
2020-07-13 17:04:32 -0300

    Exercicio 1 - operaçoes aritmeticas

---
+++ exercises/4_1/ex-1.js
@@ -0,0 +1,14 @@
+let a = 3;
+let b = 5;
+
+let soma = a + b;
+let sub = a - b;
+let mult = a * b;
+let div = a / b;
+let mod = a % b;
+
+console.log ('adição = ' + soma);
+console.log ('subtração = ' + sub);
+console.log ('multiplicação = ' + mult);
+console.log ('divisão = ' + div);
+console.log ('módulo = ' + mod);
